8deafbacbd2c47384f5c90988ee1e3f7a41b9d63
Adarsha Acharya <adarshrya@users.noreply.github.com>
2021-01-18 14:28:16 +0530

    Created UserEntity
    Crated UserEntity with User Table defination

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -0,0 +1,207 @@
+package com.upgrad.quora.service.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "USERS")
+public class UserEntity implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "firstname")
+    @NotNull
+    @Size(max = 30)
+    private String firstname;
+
+    @Column(name = "lastname")
+    @NotNull
+    @Size(max = 30)
+    private String lastname;
+
+    @Column(name = "username")
+    @NotNull
+    @Size(max = 30)
+    private String username;
+
+    @Column(name = "email")
+    @NotNull
+    @Size(max = 50)
+    private String email;
+
+    @Column(name = "password")
+    @NotNull
+    @Size(max = 255)
+    private String password;
+
+    @Column(name = "salt")
+    @NotNull
+    @Size(max = 200)
+    private String salt;
+
+    @Column(name = "country")
+    @Size(max = 30)
+    private String country;
+
+    @Column(name = "aboutme")
+    @Size(max = 50)
+    private String aboutme;
+
+    @Column(name = "dob")
+    @Size(max = 30)
+    private String dob;
+
+    @Column(name = "role")
+    @Size(max = 30)
+    private String role;
+
+    @Column(name = "contactnumber")
+    @Size(max = 30)
+    private String contactnumber;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
+    }
+
+
+    public String getLastName() {
+        return lastname;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+    public String getAboutMe() {
+        return aboutme;
+    }
+
+    public void setAboutMe(String aboutme) {
+        this.aboutme = aboutme;
+    }
+
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+    public String getContactNumber() {
+        return contactnumber;
+    }
+
+    public void setContactNumber(String contactnumber) {
+        this.contactnumber = contactnumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
