524fd42efc01459b092a8cc3a077c84cec977d20
Adarsha Acharya <adarshrya@users.noreply.github.com>
2021-01-18 14:48:04 +0530

    #3 Configure Exception Handler for SignUpRestrictedException

---
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/QuoraExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.upgrad.quora.api.exception;
+
+import com.upgrad.quora.api.model.ErrorResponse;
+import com.upgrad.quora.service.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class QuoraExceptionHandler {
+
+
+    @ExceptionHandler(SignUpRestrictedException.class)
+    public ResponseEntity<ErrorResponse> signUpRestrictedException(SignUpRestrictedException exception, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()), HttpStatus.CONFLICT
+        );
+    }
+
+}
