c7ec5331a3f10f5600f5d8dd4c006817916e078f
Sanath Swaroop <sanath15swaroop@gmail.com>
2021-01-24 10:03:16 +0530

    Create AnswerController.java
    #15 Solved

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -0,0 +1,35 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.*;
+import com.upgrad.quora.service.business.AnswerBusinessService;
+import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.exception.AnswerNotFoundException;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class AnswerController {
+
+    @Autowired
+    private AnswerBusinessService answerBusinessService;
+
+    @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerResponse> createAnswer(@RequestHeader("authorization") final String authorization, @PathVariable("questionId") final String questionId, final AnswerRequest answerRequest) throws AuthorizationFailedException, InvalidQuestionException {
+        AnswerResponse answerResponse = new AnswerResponse();
+        AnswerEntity answerEntity = answerBusinessService.createAnswer(authorization,questionId,answerRequest.getAnswer());
+        answerResponse.setId(answerEntity.getUuid());
+        answerResponse.setStatus("ANSWER CREATED");
+        return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.CREATED);
+    }
+
+   
+}
